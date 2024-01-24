cb82d1ab43bab4ff9a0a99ecc7ddbde128dbb44a
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-01-24 01:50:30 +0100

    Trying to fix hamburgermenu

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// import { hamburgerToggle } from "./ui/toggleMobileNav";
+
+// hamburgerToggle();
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+console.log(navMenu);
+
+const hamburgerToggle = hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  console.log(navMenu.classList);
+  if (navMenu.classList.contains("hidden")) {
+    navMenu.classList.remove("hidden");
+  } else {
+    navMenu.classList.add("hidden");
+  }
+});
---
+++ js/ui/toggleMobileNav.js
@@ -0,0 +1,14 @@
+// const hamburger = document.querySelector(".hamburger");
+// const navBar = document.querySelector("#navBar");
+
+// console.log(navBar);
+
+// export const hamburgerToggle = hamburger.addEventListener("click", () => {
+//   hamburger.classList.toggle("active");
+//   console.log(navBar.classList);
+//   if (navBar.classList.contains("hidden")) {
+//     navBar.classList.remove("hidden");
+//   } else {
+//     navBar.classList.add("hidden");
+//   }
+// });
