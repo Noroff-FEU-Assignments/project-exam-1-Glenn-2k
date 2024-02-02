9bf4e558df9b96c6b46db4a5668556e496ad9bcd
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-02-02 22:31:59 +0100

    trying to fetch images from api

--- js/data/constants.js
+++ js/data/constants.js
@@ -2,6 +2,8 @@ export const host = "https://cabincrafts.glennkristiansen.no";
 export const endpoint = "/wp-json/wp/v2/posts?_embed";
 export const BASE_URL = host + endpoint;
 
-export const params = new URLSearchParams(document.location.search);
+const queryString = document.location.search;
+export const params = new URLSearchParams(queryString);
 export const id = params.get("id");
-console.log(params);
+console.log(id);
+    
--- js/render/getPosts.js
+++ js/render/getPosts.js
@@ -2,33 +2,53 @@ import { fetchData } from "../data/fetchApi.js";
 
 async function dataHandler() {
   //   await fetchData().then((posts) => {
-  //     posts.forEach((post) => {
-  //       renderPosts(post);
-  //     });
-  //   });
-  const posts = await fetchData();
-  renderPosts(posts);
-}
+    //     posts.forEach((post) => {
+      //       renderPosts(post);
+      //     });
+      //   });
+      const posts = await fetchData();
+      renderPosts(posts);
+    }
+    
+    dataHandler();
 
-dataHandler();
+
+
+    function renderPosts(posts) {
+      for (let i = 0; i < posts.length; i++) {
+        const post = posts[i];
+        renderPost(post);
+      }
+    }
+    
+
+
+
 
 export async function renderPost(posts) {
   console.log(typeof posts);
   const cardContainer = document.querySelector(".blogCardContainer");
   const card = document.createElement("div");
-  card.classList.add("carouselCard");
+  card.classList.add("carouselCard", "alignCenter", "blogCardMargin");
 
-  const title = document.createElement("h3");
-  title.textContent = posts.title.rendered;
-  title.classList.add("carouselCardTitle");
 
-  card.appendChild(title);
+  const featuredMedia = posts._embedded?.['wp:featuredmedia']?.[0]?.source_url;
+
+      const img = document.createElement("img");
+      img.src = featuredMedia;
+      img.alt = posts.title.rendered;
+      img.classList.add("carouselImage");
+      card.append(img);
+
+      const cardContent = document.createElement("div");
+      cardContent.classList.add("carouselCardContent");
+      
+      const title = document.createElement("h3");
+      title.textContent = posts.title.rendered;
+      title.classList.add("carouselCardTitle");
+
+  card.appendChild(cardContent);
+  cardContent.appendChild(title);
   cardContainer.appendChild(card);
 }
 
-function renderPosts(posts) {
-  for (let i = 0; i < posts.length; i++) {
-    const post = posts[i];
-    renderPost(post);
-  }
-}
