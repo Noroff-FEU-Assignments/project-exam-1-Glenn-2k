b1ee42cf09f45ae45f0239ea2647896be5ba9284
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-02-05 10:18:30 +0100

    changed to innerHTML in excerpt

--- js/render/getPosts.js
+++ js/render/getPosts.js
@@ -2,28 +2,22 @@ import { fetchData } from "../data/fetchApi.js";
 
 async function dataHandler() {
   //   await fetchData().then((posts) => {
-    //     posts.forEach((post) => {
-      //       renderPosts(post);
-      //     });
-      //   });
-      const posts = await fetchData();
-      renderPosts(posts);
-    }
-    
-    dataHandler();
+  //     posts.forEach((post) => {
+  //       renderPosts(post);
+  //     });
+  //   });
+  const posts = await fetchData();
+  renderPosts(posts);
+}
 
+dataHandler();
 
-
-    function renderPosts(posts) {
-      for (let i = 0; i < posts.length; i++) {
-        const post = posts[i];
-        renderPost(post);
-      }
-    }
-    
-
-
-
+function renderPosts(posts) {
+  for (let i = 0; i < posts.length; i++) {
+    const post = posts[i];
+    renderPost(post);
+  }
+}
 
 export async function renderPost(posts) {
   console.log(typeof posts);
@@ -31,36 +25,33 @@ export async function renderPost(posts) {
   const card = document.createElement("div");
   card.classList.add("carouselCard", "alignCenter", "blogCardMargin");
 
+  const featuredMedia = posts._embedded?.["wp:featuredmedia"]?.[0]?.source_url;
 
-  const featuredMedia = posts._embedded?.['wp:featuredmedia']?.[0]?.source_url;
+  const img = document.createElement("img");
+  img.src = featuredMedia;
+  img.alt = posts.title.rendered;
+  img.classList.add("carouselImage");
+  card.append(img);
 
-      const img = document.createElement("img");
-      img.src = featuredMedia;
-      img.alt = posts.title.rendered;
-      img.classList.add("carouselImage");
-      card.append(img);
+  const cardContent = document.createElement("div");
+  cardContent.classList.add("carouselCardContent");
 
-      const cardContent = document.createElement("div");
-      cardContent.classList.add("carouselCardContent");
-      
-      const title = document.createElement("h3");
-      title.textContent = posts.title.rendered;
-      title.classList.add("carouselCardTitle");
+  const title = document.createElement("h3");
+  title.textContent = posts.title.rendered;
+  title.classList.add("carouselCardTitle");
 
+  const excerpt = document.createElement("p");
+  excerpt.innerHTML = posts.excerpt.rendered;
+  excerpt.classList.add("carouselCardText");
 
-      const excerpt = document.createElement("p");
-      excerpt.textContent = posts.excerpt.rendered;
-      excerpt.classList.add("carouselCardText");
+  const readMore = document.createElement("a");
+  readMore.textContent = "Read more";
+  readMore.href = `post.html?id=${posts.id}`;
+  readMore.classList.add("carouselButton");
 
-      const readMore = document.createElement("a");
-      readMore.textContent = "Read more";
-      readMore.href = `post.html?id=${posts.id}`;
-      readMore.classList.add("carouselButton");
-      
-      card.appendChild(cardContent);
-      cardContent.appendChild(title);
-      cardContent.appendChild(excerpt);    
-      cardContent.appendChild(readMore);
+  card.appendChild(cardContent);
+  cardContent.appendChild(title);
+  cardContent.appendChild(excerpt);
+  cardContent.appendChild(readMore);
   cardContainer.appendChild(card);
 }
-
