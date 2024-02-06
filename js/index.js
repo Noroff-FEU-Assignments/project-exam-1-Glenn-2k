fe8bf0f244ecd7b6c908dcb0eaecfeae638ecf34
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-02-06 11:10:38 +0100

    added dragging action to slider

--- js/index.js
+++ js/index.js
@@ -43,3 +43,30 @@ switch (window.location.pathname) {
     console.log("404 - not found");
     break;
 }
+
+const slider = document.querySelector(".carouselWrapper");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; // The number 3 determines the sensitivity of the drag
+  slider.scrollLeft = scrollLeft - walk;
+});
