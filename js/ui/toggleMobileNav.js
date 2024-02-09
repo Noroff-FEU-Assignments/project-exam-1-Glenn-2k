74394e7d43300bdb96d1310936bf8c1503d65e97
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-02-09 10:26:50 +0100

    Added rottating hamburger

--- js/ui/toggleMobileNav.js
+++ js/ui/toggleMobileNav.js
@@ -1,15 +1,13 @@
 export async function hamburgerMenu() {
   const hamburger = document.querySelector(".hamburger");
   const navMenu = document.querySelector(".nav-menu");
+  const hamburgerRotate = hamburger.querySelector(".HamburgerPins");
 
   hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
-    hamburger.classList.toggle("rotate");
+    hamburgerRotate.classList.toggle("rotate");
     console.log(navMenu.classList);
-    if (navMenu.classList.contains("hidden")) {
-      navMenu.classList.remove("hidden");
-    } else {
-      navMenu.classList.add("hidden");
-    }
+
+    navMenu.classList.toggle("hidden");
   });
 }
