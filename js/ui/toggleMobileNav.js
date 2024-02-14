08b5386b8985d4376b980975646c4a0c123ee5f4
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-02-14 12:46:19 +0100

    added functionality to hamburger.

--- js/ui/toggleMobileNav.js
+++ js/ui/toggleMobileNav.js
@@ -3,9 +3,19 @@ export async function hamburgerMenu() {
   const navMenu = document.querySelector(".nav-menu");
   const hamburgerRotate = hamburger.querySelector(".HamburgerPins");
 
-  hamburger.addEventListener("click", () => {
+  hamburger.addEventListener("click", (event) => {
     hamburger.classList.toggle("active");
     hamburgerRotate.classList.toggle("rotate");
     navMenu.classList.toggle("hidden");
+
+    event.stopPropagation();
+  });
+
+  document.addEventListener("click", function (event) {
+    if (!hamburger.contains(event.target) && !navMenu.contains(event.target)) {
+      hamburger.classList.remove("active");
+      hamburgerRotate.classList.remove("rotate");
+      navMenu.classList.add("hidden");
+    }
   });
 }
